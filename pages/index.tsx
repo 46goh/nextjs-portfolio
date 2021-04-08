b5897eeb6941cf264845d8f6dca116aa8455ea8a
ta-shin <palcum@gmail.com>
2021-04-08 20:38:52 +0900

    add typescript, eslint, prettier

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Tatsuya Shinjo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Tatsuya Shinjo</h1>
+      </main>
+    </div>
+  );
+}
